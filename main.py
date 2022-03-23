3051ce811c8fbf8683c2c7dbbd3c64174d545d99
tuliod12 <102202291+tuliod12@users.noreply.github.com>
2022-03-23 11:59:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import mysql.connector
 
 # Initialize connection.
 # Uses st.cache to only run once.
-@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+
 def init_connection():
     return mysql.connector.connect(**st.secrets["mysql"])
 
